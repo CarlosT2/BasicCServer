b1f2b1e215e8984d00d0a1394559096f8a2eb2ff
CarlosT2 <163529777+CarlosT2@users.noreply.github.com>
2024-03-15 09:21:31 -0300

    Update Main.c

--- Client/Main.c
+++ Client/Main.c
@@ -1,5 +1,10 @@
 #include <winsock2.h>
 #include <winsock.h>
+#include "Utilities.h"
+#define Myport1 0x7f000001
+#define MyPort 1337
 int main(int argc, char** argv){
-    
+    My_WsaStart();
+    struct SOCKADDR_IN = createSockaddrin(
+    MyPort, AF_INET, MyPort1);
 }
