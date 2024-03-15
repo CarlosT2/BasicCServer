beb5095e245ea46aa10ecec8fc9f1ae5279b2579
CarlosT2 <163529777+CarlosT2@users.noreply.github.com>
2024-03-15 09:21:24 -0300

    Update Utilities.h

--- Client/Utilities.h
+++ Client/Utilities.h
@@ -2,6 +2,9 @@
 #define Utilities
 #include <stdlib.h>
 #include <string.h>
+
+#include <winsock2.h>
+#include <winsock.h>
 void my_WsaStart(){
     WSA wsa = {0};
     if(0>WsaStartup( MakeWord(2,2),
@@ -9,5 +12,7 @@ void my_WsaStart(){
         printf("noWsa");
         exit(-1);
 }
-
+struct SOCKADDR_IN createSOCKADDR_IN(
+int sin_addr, int family, saddr){
+    SOCKADDR_IN sockaddrin
 #endif
